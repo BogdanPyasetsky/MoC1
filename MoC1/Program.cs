3c9e7d91e09f50bfe7c47c02a7a957086c26bd2e
BogdanPyasetsky <38696453+BogdanPyasetsky@users.noreply.github.com>
2020-10-21 13:16:16 +0300

    Done functions for probability calculations

--- MoC1/Program.cs
+++ MoC1/Program.cs
@@ -37,7 +37,52 @@ namespace MoC1
             }
         }
 
-       
+        static float[] CProbCalc(float[] MProb, float[]KProb, int[,] EncrTable)
+        {
+            float[] CProb = new float[20];
+            int cpIndex;
+            for (int i = 0; i < 20; i++)
+            {
+                for (int j = 0; j < 20; j++)
+                {
+                    cpIndex = EncrTable[i, j];
+                    CProb[cpIndex] += MProb[i] * KProb[j];
+                }
+            }           
+            return CProb;
+        }
+
+        static float[,] MCProbCalc(float[] MProb, float[] KProb, int[,] EncrTable)
+        {
+            float[,] CMProb = new float[20, 20];
+            int cpIndex;
+            for (int i = 0; i < 20; i++)
+            {
+                for (int j = 0; j < 20; j++)
+                {
+                    cpIndex = EncrTable[i, j];
+                    CMProb[i, cpIndex] += MProb[i] * KProb[j];
+                }
+            }
+
+
+            return CMProb;
+        }
+
+        static float[,] MCCondProbCalc(float[] CProb, float[,] MCProb)
+        {
+            float[,] MCcondProb = new float[20, 20];
+            for (int i = 0; i < 20; i++)
+            {
+                for (int j = 0; j < 20; j++)
+                {
+                    MCcondProb[i, j] = MCProb[i, j] / CProb[j];
+                }
+            }
+            return MCcondProb;
+        }
+
+
         static void Main(string[] args)
         {
             float[] MP = new float[20];
@@ -45,10 +90,17 @@ namespace MoC1
             int[,] ET = new int[20, 20];
             string v = "06";
             ReadFiles(v, MP, KP, ET);
-
-
-            Console.WriteLine(ET[3, 8]);
-
+            var MCP = MCProbCalc(MP, KP, ET);
+            float t = 0;
+            for (int i = 0; i < 20; i++)
+            {
+                for (int j = 0; j < 20; j++)
+                {
+                    Console.Write(MCP[i, j] + "  ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine(t);
             Console.ReadKey();
         }
     }
